048b6df687a0da75af333bdbe5eab8f5f270596f
Jhoncel Nayve <jhoncelnayve@gmail.com>
2025-05-02 11:16:29 +0000

    Fix missing alias in vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import react from '@vitejs/plugin-react';
+import path from 'path';
 import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
@@ -6,5 +7,10 @@ export default defineConfig({
     plugins: [react()],
     server: {
         host: '127.0.0.1',
+    },
+    resolve: {
+        alias: {
+            src: path.resolve(__dirname, 'src'),
+        },
     },
 });
